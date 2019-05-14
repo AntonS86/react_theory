77e7108d126e2dbe74c6e41ff15d51893b68b5d0
antonS <so4usertrue@gmail.com>
2019-05-14 21:14:00 +0300

    add react app

---
+++ src/Car/Car.js
@@ -0,0 +1,25 @@
+import React from 'react';
+/*function Car() {
+    return (
+        <h2>This is car Component</h2>
+    )
+}*/
+
+/*const car = () => {
+    return (
+        <h2>This is car Component</h2>
+    )
+}*/
+
+/*const car = () => (
+    <div>
+        This is car Component
+        <span>Test</span>
+    </div>
+);*/
+export default () => (
+    <div>
+        <p>This is car Component</p>
+        <p><strong>{1+1}</strong></p>
+    </div>
+);
