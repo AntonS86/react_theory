4a0e7a9ce27653e56f246cc4208cd05d3c42577d
antonS <so4usertrue@gmail.com>
2019-05-15 11:55:01 +0300

    block 4: less 3

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -18,11 +18,27 @@ import './Car.css';
         <span>Test</span>
     </div>
 );*/
-export default props => (
-    <div className="car-card">
-        <p>Car name: {props.name}</p>
-        <p>Year: <strong>{props.year}</strong></p>
-        <input type="text" onChange={props.onChangeName} value={props.name}/>
-        <button onClick={props.onDelete}>Delete</button>
-    </div>
-);
+export default props => {
+    const inputClasses = ['input'];
+    if (props.name !== '') {
+        inputClasses.push('green');
+    } else {
+        inputClasses.push('red');
+    }
+
+    if (props.name.length > 4) {
+        inputClasses.push('bold');
+    }
+    return (
+        <div className="car-card">
+            <p>Car name: {props.name}</p>
+            <p>Year: <strong>{props.year}</strong></p>
+            <input type="text"
+                   onChange={props.onChangeName}
+                   value={props.name}
+                   className={inputClasses.join(' ')}
+            />
+            <button onClick={props.onDelete}>Delete</button>
+        </div>
+    )
+}
